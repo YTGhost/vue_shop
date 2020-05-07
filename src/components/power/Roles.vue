09f37c9a15492a6873addd341f4fb9e42a2fb441
YTGhost <283304489@qq.com>
2020-05-07 13:02:31 +0800

    完成角色列表中权限列表 2020/05/07

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -19,7 +19,32 @@
         <!--角色列表区域-->
         <el-table :data="rolelist" border stripe>
           <!--展开列-->
-          <el-table-column type="expand"></el-table-column>
+          <el-table-column type="expand">
+            <template slot-scope="scope">
+              <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+                <!--渲染一级权限-->
+                <el-col :span="5">
+                  <el-tag closable @close="removeRightById(scope.row, item1.id)">{{item1.authName}}</el-tag>
+                  <i class="el-icon-caret-right"></i>
+                </el-col>
+                <!--渲染二级和三级权限-->
+                <el-col :span="19">
+                  <!--通过for循环嵌套渲染二级权限-->
+                  <el-row :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                    <el-col :span="6">
+                      <el-tag type="success" closable @close="removeRightById(scope.row, item2.id)">{{item2.authName}}</el-tag>
+                      <i class="el-icon-caret-right"></i>
+                    </el-col>
+                    <el-col :span="18">
+                      <el-tag type="warning" v-for="item3 in item2.children" :key="item3.id" closable @close="removeRightById(scope.row, item3.id)">
+                        {{item3.authName}}
+                      </el-tag>
+                    </el-col>
+                  </el-row>
+                </el-col>
+              </el-row>
+            </template>
+          </el-table-column>
           <!--索引列-->
           <el-table-column type="index">#</el-table-column>
           <el-table-column label="角色名称" prop="roleName"></el-table-column>
@@ -53,11 +78,44 @@ export default {
       const { data: res } = await this.$http.get('roles')
       if (res.meta.status !== 200) return this.$message.error('获取角色列表失败！')
       this.rolelist = res.data
+    },
+    // 根据Id删除对应的权限
+    async removeRightById (role, rightId) {
+      // 弹框提示用户是否要删除
+      const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消了删除！')
+      }
+      const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除权限失败！')
+      }
+      role.children = res.data
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+  .el-tag {
+    margin: 7px;
+  }
+
+  .bdtop {
+    border-top: 1px solid #eeeeee;
+  }
+
+  .bdbottom {
+    border-bottom: 1px solid #eeeeee;
+  }
+
+  .vcenter {
+    display: flex;
+    align-items: center;
+  }
 
 </style>
